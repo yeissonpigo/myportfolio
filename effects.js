e23f5085b3d228d6f4854deed2bd6ded7562ba9c
Yeisson Pinilla <yeissonpigo@unisabana.edu.co>
2021-05-26 12:12:47 +0300

    Esta es la versión 0.2 de mi portafolio

--- effects.js
+++ effects.js
@@ -14,9 +14,27 @@ window.addEventListener("scroll", function () {
   var currentPosition = aboutme.getBoundingClientRect().top;
   var puntoDeAparicion = 50;
 
-  if(currentPosition < (windowheight - puntoDeAparicion)) {
-      aboutme.classList.add('show');
+  if (currentPosition < windowheight - puntoDeAparicion) {
+    aboutme.classList.add("show");
   } else {
-      aboutme.classList.remove('show');
+    aboutme.classList.remove("show");
   }
 });
+
+//Efecto del menú burger
+function navSlide() {
+  const burger = document.querySelector(".navbar__burger");
+  const navlink = document.querySelector(".list");
+  var flag = 0;
+  burger.addEventListener("click", () => {
+    if (flag === 0) {
+      navlink.style.left = "50%";
+      flag = 1;
+    } else {
+      navlink.style.left = "200%";
+      flag = 0;
+    }
+  });
+}
+
+navSlide();
